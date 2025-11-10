152d4966cb1f3f1e9612db61d96a6ceade6696c8
tswt0825 <tswt0825@gmail.com>
2025-11-10 10:31:52 +0100

    hei

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bistro-northern-lights/",
+  base: "/bistro-northern-lights/", // 👈 Dette er den viktige linja!
 });
